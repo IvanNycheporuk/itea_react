a0881c6a7d2dc758fb818903c0fdd6d6bc93c922
Ivan Nycheporuk <ivan.nycheporuk@gmail.com>
2020-07-02 12:38:55 +0300

    completed task1

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -1,9 +1,23 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Chart from 'chart.js';
 
-
 const ChartComponent = () => {
+    const [ chartData, setChartData ] = useState([0, 10, 5, 2, 20, 30, 45])
     const chart = useRef(null);
+
+    const randomInteger = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const randomizeData = ( count, min, max ) => () => {
+        let randomArr = [];
+
+        for (let i = 0; i < count; i++) {
+            randomArr.push( randomInteger(min, max) );
+        }
+
+        setChartData( randomArr );
+    }
 
     useEffect(() => {
         console.log(chart)
@@ -18,17 +32,17 @@ const ChartComponent = () => {
                     label: 'My First dataset',
                     backgroundColor: 'rgb(255, 99, 132)',
                     borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45]
+                    data: chartData
                 }]
             },
             options: {}
           });
-    }, [])
+    }, [chartData])
 
     return (
         <div className="chart-wrap">
             <canvas className="chart" ref={ chart }> test </canvas>
-            <button> Randomize </button>
+            <button onClick={ randomizeData(7, 0 , 100) }> Randomize </button>
         </div>
     )
 }
